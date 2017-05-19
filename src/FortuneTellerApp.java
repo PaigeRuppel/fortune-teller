df07d2e2044556ed342a0fbb7dcf83c864b3d21c
Paige Ruppel <paige.ruppel@gmail.com>
2017-05-19 15:03:05 -0400

    working draft

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -11,40 +11,32 @@ public class FortuneTellerApp {
 				"I will ask you a series of questions to discern your fortune. Only honest answers will provide your true fortune.");
 		System.out.println(
 				"If at any time you wish to quit this program, simply type \"quit\" - knowing your future is not for the faint of heart!");
-		System.out.println("Are you ready to begin? (type \"yes\" or \"no\")");
-		String beginAnswer = input.next();
-		if (beginAnswer.toLowerCase().equals("no")) {
-			System.out.println("Come back when you are ready to seek the truth!");
-		} else {
 
-			System.out.println("Please enter your first name:"); // ask the user
-																	// for
-																	// their
-																	// first
-																	// name
+	
+		// input.findInLine;
+
+		// while all initial responses do not equal quit (ignore case) keep
+		// running the program
+		// if any initial response equals quit (ignore case) the next output
+		// should be the quitter statement
+		int q = 0;
+		while (q == 0) {
+			// ask the user for their first and last name
+			System.out.println("Please enter your first name:");
 			String firstName = input.next();
-
-			System.out.println("Please enter your last name:"); // ask the user
-																// for
-																// their last
-																// name
+			System.out.println("Please enter your last name:");
 			String lastName = input.next();
-
-			System.out.println("Please enter your age:"); // ask the user for
-															// their
-															// age
+			// ask the user for their age
+			System.out.println("Hello, " + firstName + " " + lastName + ". Please enter your age:");
 			int userAge = input.nextInt();
-
 			// ask the user for their birth month (as an int)
 			System.out.println(
 					"Please enter your birth month (enter as a number, i.e if you were born in May, you should enter \"5\"):");
 			int birthMonth = input.nextInt();
-
-			// ask the user for their favorite ROYGBIV color (if user does not
-			// know
-			// ROYGBIV, give Help option)
+			// ask the user for their favorite ROYGBIV color (provide help
+			// option)
 			System.out.println(
-					"Please enter your favorite ROYGBIV color (enter \"help\" if you do not understand ROYGBIV:)");
+					"Please enter your favorite ROYGBIV color (enter \"help\" if you do not understand ROYGBIV):");
 			String favColor = input.next();
 			int numSiblings = 0;
 			switch (favColor) {
@@ -60,10 +52,7 @@ public class FortuneTellerApp {
 				numSiblings = input.nextInt();
 			}
 
-			// If the user’s age is an odd number, then the user will retire in
-			// ____
-			// years if the user's age is odd, or ___ years if user's age is
-			// even.
+			// Give retirement years based on if user's age is even or odd
 			int retireYears;
 			if (userAge % 2 == 0) {
 				retireYears = 15;
@@ -75,10 +64,9 @@ public class FortuneTellerApp {
 			// bank;
 			// if the user's birth month is 5-8, they will have $____ in the
 			// bank;
-			// and if it is 9-12, then they will have $____ in the bank.
+			// and if it is 9-12, then they will have $____ in the bank
 			// If the user enters something other than 1-12 as their birth
-			// month,
-			// they will have $0.00 in the bank.
+			// month, they will have $0.00 in the bank.
 			String bankMoney;
 			switch (birthMonth) {
 			case 1:
@@ -103,17 +91,8 @@ public class FortuneTellerApp {
 				bankMoney = "$0.00";
 			}
 
-			// If the user’s number of siblings is zero, then they will have a
-			// vacation home in ____ (location),
-			// if one then they will have a vacation home in ____ (location),
-			// if two then they will have a vacation home in ____ (location),
-			// if three then they will have a vacation home in ____ (location),
-			// or if more than three then they will have a vacation home in ____
-			// (location).
-			// If the user enters anything other than a number greater than or
-			// equal
-			// to 0, they should get a bad vacation home!
-
+			// Give vacation home location based on number of siblings
+			// (1,2,3,>=4,other returns bad vacay spot)
 			String vacationHome;
 			if (numSiblings == 0) {
 				vacationHome = "in Budapest";
@@ -129,10 +108,7 @@ public class FortuneTellerApp {
 				vacationHome = "at the bottom of the Grove City, OH landfill.";
 			}
 
-			// Depending on which ROYGBIV color is the user’s favorite, they
-			// will
-			// have a specific mode of transportation (i.e. car, boat, plane,
-			// etc.).
+			// give mode of transportation based on favorite color
 			String transportation;
 			if (favColor.toLowerCase().equals("red")) {
 				transportation = "Corvette";
@@ -158,9 +134,10 @@ public class FortuneTellerApp {
 			// Money] in the bank, a vacation home in [Location] and a [Mode of
 			// Transportation].
 			System.out.println(firstName + " " + lastName + " will retire in " + retireYears + " years with "
-					+ bankMoney + " in the bank, a vacation home " + vacationHome + ", and a " + transportation + ".");
+					+ bankMoney + " in the bank, a vacation home " + vacationHome + ", and travel by a "
+					+ transportation + ".");
 			input.close();
 		}
 	}
-
 }
+
